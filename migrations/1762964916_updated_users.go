03e8125975c4643f79ec017fd1ff390147de8d5c
Vivien Alvarez <viennealvarez22@gmail.com>
2025-11-13 00:28:58 +0800

    feat: add role field to user collection with selectable options

---
+++ migrations/1762964916_updated_users.go
@@ -0,0 +1,63 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(5, []byte(`{
+			"hidden": false,
+			"id": "select1466534506",
+			"maxSelect": 1,
+			"name": "role",
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "select",
+			"values": [
+				"Administrator",
+				"Building Admin",
+				"Tenant",
+				"Automation"
+			]
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(5, []byte(`{
+			"hidden": false,
+			"id": "select1466534506",
+			"maxSelect": 1,
+			"name": "role",
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "select",
+			"values": [
+				"Administrator",
+				"Building Admin",
+				"Tenant"
+			]
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
