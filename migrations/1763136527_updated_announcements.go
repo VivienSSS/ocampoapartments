2558a16c782a463fca677814b62bdcca7912f767
Vivien Alvarez <viennealvarez22@gmail.com>
2025-11-15 00:10:02 +0800

    feat: add migration to include 'hasSent' boolean field in multiple collections

---
+++ migrations/1763136527_updated_announcements.go
@@ -0,0 +1,40 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3866499052")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(4, []byte(`{
+			"hidden": false,
+			"id": "bool1667088791",
+			"name": "hasSent",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "bool"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3866499052")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("bool1667088791")
+
+		return app.Save(collection)
+	})
+}
